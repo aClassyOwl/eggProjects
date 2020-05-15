27860634922ae4505e6e2d300b9ed66a12f2fae8
aClassyOwl <65378588+aClassyOwl@users.noreply.github.com>
2020-05-15 12:45:02 -0400

    Update and rename EggWow.java to EggProjects.java
    Updated naming

--- EggProjects.java
+++ EggProjects.java
@@ -1,8 +1,8 @@
-package app.jpek.dev.eggwow;
+package app.jpek.dev.eggprojects;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EggWow extends JavaPlugin {
+public final class EggProjects extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
